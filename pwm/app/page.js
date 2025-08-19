4c402da980851271911dc20d2b47884126e3e952
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2025-08-19 15:27:21 -0300

    Update page.js

--- pwm/app/page.js
+++ pwm/app/page.js
@@ -3,8 +3,27 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Olá turma</h1>
-    </div>
+    <main className={styles.main}>
+      <div className={styles.profile}>
+        {/* Foto */}
+        <Image
+          src="/me.png"
+          alt="Minha foto"
+          width={200}
+          height={200}
+          className={styles.avatar}
+        />
+
+        {/* Nome */}
+        <h1 className={styles.title}>Antônio Edson Alves de Holanda Neto</h1>
+
+        {/* Mini bio */}
+        <p className={styles.bio}>
+          Sou estudante de Ciência da Computação na UNICAP.  
+          Atualmente estou no sexto período.  
+          No tempo livre gosto de jogar e ir a academia.
+        </p>
+      </div>
+    </main>
   );
 }
