e19ed5d80979d60f3716b2762151bb9d109b3083
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2025-08-22 07:43:47 -0300

    Update page.js

--- pwm/app/page.js
+++ pwm/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.profile}>
         {/* Foto */}
         <Image
-          src="/me.png"
+          src="https://media.licdn.com/dms/image/v2/D4D03AQEujvr8jfNNEA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1698277975626?e=1758758400&v=beta&t=3uB9XBEOOTMnsB0ep2QePC_6RXVCMUzX4blhLOSVE5U"
           alt="Minha foto"
           width={200}
           height={200}
