322579a1814fbf556e6fc9916c1bf280356f2269
PheonixEdson <135137092+PheonixEdson@users.noreply.github.com>
2025-08-22 07:46:22 -0300

    Update page.js

--- pwm/app/page.js
+++ pwm/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         {/* Foto */}
         <Image
           src="https://media.licdn.com/dms/image/v2/D4D03AQEujvr8jfNNEA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1698277975626?e=1758758400&v=beta&t=3uB9XBEOOTMnsB0ep2QePC_6RXVCMUzX4blhLOSVE5U"
-          alt="Minha foto"
+          alt="Foto de perfil de Antônio Edson Alves de Holanda Neto"
           width={200}
           height={200}
           className={styles.avatar}
@@ -21,7 +21,7 @@ export default function Home() {
         <p className={styles.bio}>
           Sou estudante de Ciência da Computação na UNICAP.  
           Atualmente estou no sexto período.  
-          No tempo livre gosto de jogar e ir a academia.
+          No tempo livre gosto de jogar e ir à academia.
         </p>
       </div>
     </main>
